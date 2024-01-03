215d37c45600562dadfa761e8a5447a09ec05ff5
a2thedev <anthonygreen048@gmail.com>
2024-01-03 17:03:09 -0500

    created TextField, NewExpenseForm, NewIncomeForm, and FullCalendar

---
+++ src/components/FullCalendar/FullCalendar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+function Calendar() {
+  return (
+    <div>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView={"dayGridMonth"}
+        headerToolbar={{
+          start: "dayGridMonth,timeGridWeek,timeGridDay",
+          center: "title",
+          end: "today prev, next",
+        }}
+      />
+    </div>
+  );
+}
+
+export default Calendar;
---
+++ src/components/NewExpenseForm/NewExpenseForm.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import TextField from "../TextField/TextField";
+import axios from "axios";
+
+const NewExpenseForm = ({ onNewExpense }) => {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+  const [rating, setRating] = useState("");
+  const [isPaid, setIsPaid] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = {
+      name,
+      amount,
+      date,
+      rating,
+      isPaid,
+    };
+    try {
+      const response = await axios.post(
+        "https://localhost:5001/api/Expenses",
+        formData
+      );
+      if (response.status === 201) {
+        onNewExpense();
+      }
+    } catch (error) {
+      console.warn("error submitting new expense form:", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="add-style">
+      <h4>Add Expense</h4>
+      <div className="p-2">
+        <TextField label="Name" value={name} onChange={setName} />
+        <TextField label="Amount" value={amount} onChange={setAmount} />
+        <TextField
+          label="Date (ex: year-month-day)"
+          value={date}
+          onChange={setDate}
+        />
+        <TextField label="Rating" value={rating} onChange={setRating} />
+        <TextField label="IsPaid" value={isPaid} onChange={setIsPaid} />
+        <div className="d-flex justify-content-center">
+          <button className="btn btn-outline-success" type="submit">
+            Add Expense
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default NewExpenseForm;
