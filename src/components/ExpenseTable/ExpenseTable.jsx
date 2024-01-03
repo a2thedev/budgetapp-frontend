02bef0e87144c9a4edf59de5ca4e6e538b064c7e
a2thedev <anthonygreen048@gmail.com>
2024-01-03 18:36:06 -0500

    created ExpenseTable

---
+++ src/components/ExpenseTable/ExpenseTable.jsx
@@ -0,0 +1,32 @@
+import ExpenseItem from "../ExpenseItem/ExpenseItem";
+import "./ExpenseItem.css";
+
+const ExpenseTable = ({ expense = [], activeIndex, setActiveIndex }) => {
+  const expenseItems = expense.map((expense, i) => (
+    <ExpenseItem
+      expense={expense}
+      key={expense.id}
+      activeIndex={activeIndex}
+      setActiveIndex={setActiveIndex}
+      index={i}
+    />
+  ));
+
+  return (
+    <table className="table-success flex-item">
+      <thead>
+        <tr>
+          <th className="header-cell">Expense</th>
+          <th className="header-cell"> Amount </th>
+          <th className="header-cell"> Date</th>
+          <th className="header-cell"> Rating</th>
+          <th className="header-cell">Paid</th>
+          <th className="header-cell">Delete Expense</th>
+        </tr>
+      </thead>
+      <tbody>{expenseItems}</tbody>
+    </table>
+  );
+};
+
+export default ExpenseTable;
